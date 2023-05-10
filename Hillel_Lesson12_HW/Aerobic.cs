1ae8e7983c39125c03386b6681d68ba9cbb332c3
R41NB0WD4SH <116415348+R41NB0WD4SH@users.noreply.github.com>
2023-05-11 02:55:31 +0300

    1

---
+++ Hillel_Lesson12_HW/Aerobic.cs
@@ -0,0 +1,8 @@
+namespace Hillel_Lesson12_HW;
+
+public abstract class Aerobic : Animal
+{
+
+    public abstract void ShowOxygenAlert();
+
+}
---
+++ Hillel_Lesson12_HW/Animal.cs
@@ -0,0 +1,58 @@
+namespace Hillel_Lesson12_HW;
+
+public class Animal
+{
+
+    protected string _typeOfAnimal;
+    protected string _name;
+    protected int _age;
+
+    internal string TypeOfAnimal
+    {
+        get
+        {
+            return _typeOfAnimal;
+        }
+        
+    }
+    internal string Name
+    {
+        get { return _name; }
+        set { _name = value; }
+    }
+    
+
+    internal int Age
+    {
+        get { return _age; }
+        set
+        {
+            if (value >= 0)
+            {
+                _age = value;
+            }
+            else
+            {
+                _age = 0;
+            }
+        }
+    }
+
+
+    public Animal(string name, int age)
+    {
+        _name = name;
+        _age = age;
+    }
+
+    public Animal()
+    {
+        
+    }
+
+    public virtual void MakeSound()
+    {
+        Console.WriteLine("");
+    }
+    
+}
